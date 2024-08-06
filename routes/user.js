60e8bb7d72f37302775b1dcb2f126b049dc02fb4
jaideepdnaik <jaideep.d@juegostudioz.com>
2024-08-06 11:06:59 +0530

    Created a user.js file which will handle all the routes related to users.

---
+++ routes/user.js
@@ -0,0 +1,23 @@
+const { Router } = requrie('express');
+const User = require("../models/user");
+
+const router = Router();
+
+router.get("/signin", (req, res) => {
+    return res.render("signin");
+});
+
+router.get("/signup", (req, res) => {
+    return res.render("signup");
+});
+
+router.post("/signup", async(req, res) => {
+    const { fullName, email, password } = req.body;
+    await User.create({
+        fullName,
+        email,
+        password,
+    });
+});
+
+module.exports = router;
