b6a25f23166270af10ca5ef26d60f000a07baaac
jaideepdnaik <jaideep.d@juegostudioz.com>
2024-08-06 11:53:18 +0530

    Made new changes regarding pasword hashing

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+const userRoute = require("./routes/user");
 
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect("mongodb://localhost:27017/blogify")
+    .then(e => console.log("Connected to MongoDB"));
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve("./views"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
 });
+
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const { Router } = requrie('express');
+const { Router } = require('express');
 const User = require("../models/user");
 
 const router = Router();
@@ -11,6 +11,14 @@ router.get("/signup", (req, res) => {
     return res.render("signup");
 });
 
+router.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+    const user = User.matchPassword(email, password);
+
+    console.log('User', user);
+    return res.redirect("/");
+});
+
 router.post("/signup", async(req, res) => {
     const { fullName, email, password } = req.body;
     await User.create({
@@ -18,6 +26,7 @@ router.post("/signup", async(req, res) => {
         email,
         password,
     });
+    return res.redirect("/");
 });
 
 module.exports = router;
